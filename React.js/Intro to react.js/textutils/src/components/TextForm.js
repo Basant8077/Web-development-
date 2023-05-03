d984929228c2c80e78b73b7105f9e35a9c1cd62d
Basant joshi <bsntballabhjoshi@gmail.com>
2023-05-03 23:29:50 +0530

    Completed TextForm.js and entered some new features

--- React.js/Intro to react.js/textutils/src/components/TextForm.js
+++ React.js/Intro to react.js/textutils/src/components/TextForm.js
@@ -1,31 +1,107 @@
 import { useState } from 'react'
-import React from 'react'
+import React from 'react'  // rfc  react function based compnent
 import PropTypes from 'prop-types'
 
 
 export default function TextForm(props) {
-    const handleonchange =(event)=>{
-    
+    //! Features
+    const handlelower = () => {
+        let lower_Text = text.toLowerCase();
+        setText(lower_Text);
+    }
+    const findcharcters = () => {
+        let str = text.trim();
+        return str.length;
+    }
+    const findSyllabul =()=>{
+        let syllabul = text.split('')
+        let count=0;
+        for (let i = 0; i < syllabul.length; i++) {
+            if(
+                syllabul[i]==='a'|| syllabul[i]==='e' || syllabul[i]==='i'|| syllabul[i]==='o'|| syllabul[i]==='u'|| syllabul[i]==='A'|| syllabul[i]==='E'|| syllabul[i]==='I' || syllabul[i]==='O'|| syllabul[i]==='U'
+            )
+            {
+                count++;
+            }
+            
+        }
+        return count;
+        
+    }
+    const sentence = ()=>{
+
+        let data = text.split('');
+        let count = 0;
+        for(let i =0 ; i < data.length ; i++)
+        {
+            if(data[i]==='.')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    const display =()=>{
+        let a = findSyllabul();
+        return a;
+    }
+    const displaysentence=()=>{
+        let a = sentence();
+        return a;
+    }
+
+
+
+
+    //! Buttons 
+    const handleonchange = (event) => {
+
         setText(event.target.value)
 
     }
-    const handleonclick =()=>{
-       
+    const handleonclick = () => {
+
         let newText = text.toUpperCase();
-        setText(newText )
+        setText(newText)
+    }
+
+    const handleclear = () => {
+        setText("");
     }
 
 
-    const [text, setText] = useState('Enter Text here');
+
+    const [text, setText] = useState('');
     //* text="Change the text" wrong way to change the state
     // setText("New Text"); //! correct way to set a text
     return (
-        <div className="container my-4 ">
-            <h3>{props.Heading}</h3>
-            <textarea className="form-control my-3" value={text
-            } onChange={handleonchange} id="exampleFormControlTextarea1" rows="8"></textarea>
-            <button className='btn btn-primary' onClick={handleonclick} >Convert to Uppercase</button>
-        </div>
+        <>
+            <div className="container my-4 ">
+                <h3>{props.Heading}</h3>
+                <textarea className="form-control my-3" value={text
+                } onChange={handleonchange} id="exampleFormControlTextarea1" rows="5"></textarea>
+                <button className='btn btn-outline-primary' onClick={handleonclick} >Convert to Uppercase</button>
+                <button className='btn btn-outline-primary mx-3' onClick={handlelower} >Convert to Lowercase</button>
+                <button className='btn btn-danger mx-3' onClick={handleclear} >Clear All</button>
+            </div>
+
+
+            <div className="container">
+                <h1>Your Text summary</h1>
+                <div className="container my-4">
+                    <p>1) Number of characters (without spaces) = <b>{findcharcters()}</b>  </p>
+                    <p>2) Number of characters (with spaces) = <b>{text.length}</b></p>
+                    <p>3) Number of Words = <b>{text.split(" ").length}</b>  </p>
+                    <p>4) Number of syllabuls = <b>{display()}</b> </p>
+                    <p>5) Sentences = <b>{displaysentence()}</b></p>
+                    <p> Time take to read = <b>{(text.length * 0.087).toFixed(3)} Seconds , {(text.length * 0.001464).toFixed(6)} Minutes</b> </p>
+
+                    <h3>Preview of text</h3>
+                    <p>{text}</p>
+                </div>
+            </div>
+        </>
 
     )
 }
