602ac0e00ef7ad1a18899085af05db562463e3b2
Basant joshi <bsntballabhjoshi@gmail.com>
2023-07-08 13:21:32 +0530

    Infnite scroller added

--- React.js/insorts/src/Components/News.js
+++ React.js/insorts/src/Components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
 
@@ -9,7 +10,7 @@ export default class News extends Component {
     super(props);
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
       totalResults: 1,
 
@@ -17,89 +18,127 @@ export default class News extends Component {
     }
     document.title = `${this.capitalizeFullString(this.props.category)} - ${this.capitalizeFullString("Inshort's")} `
   }
-   capitalizeFullString=(string) =>{
+  capitalizeFullString = (string) => {
     if (typeof string !== 'string') {
       throw new Error('Input must be a string');
     }
-  
+
     if (string.length === 0) {
       return '';
     }
-  
+
     const words = string.split(' ');
     const capitalizedWords = words.map(word => {
       return word.charAt(0).toUpperCase() + word.slice(1);
     });
-  
+
     return capitalizedWords.join(' ');
   }
-  
-  async ubdate(){
+
+  async ubdate() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=70c24b93845f4b2395e274ae78fa4a4f&page=${this.state.page}&pagesize=${this.props.pagesize}`
-    this.setState({ loading: true })
+    // this.setState({ loading: true })
     let data = await fetch(url);
     let parseddata = await data.json();
     this.setState({
       articles: parseddata.articles,
       totalResults: parseddata.totalResults,
       loading: false
+      
     })
   }
   async componentDidMount() {
     this.ubdate();
 
   }
-  handlenextclick = async () => {
-    if ( this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pagesize)) {
-      
-    }
-    else{
+  fetchMoreData = async () => {
+    setTimeout(async()=>{
+      this.setState({
+        page: this.state.page + 1
+      });
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=70c24b93845f4b2395e274ae78fa4a4f&page=${this.state.page}&pagesize=${this.props.pagesize}`
+   
+    let data = await fetch(url);
+    let parseddata = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parseddata.articles),
+      totalResults: parseddata.totalResults,
+      loading: false
     
-      this.setState({page: this.state.page+1})
-      this.ubdate();
-    }
+    })
+    },3000)
     
-  }
-  handlepreviousclick = async () => {
+
     
-    this.setState({page: this.state.page -1})
-    this.ubdate();
-  }
+
+  };
+
+  // handlenextclick = async () => {
+  //   if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pagesize)) {
+
+  //   }
+  //   else {
+
+  //     this.setState({ page: this.state.page + 1 })
+  //     this.ubdate();
+  //   }
+
+  // }
+  // handlepreviousclick = async () => {
+
+  //   this.setState({ page: this.state.page - 1 })
+  //   this.ubdate();
+  // }
 
   render() {
     return (
+
       <div style={{ backgroundColor: "#202020", color: "white" }}>
-        <div className="container">
-          <h1 className="text-center">Inshort's - Top headlines on {this.props.category}</h1>
-          {this.state.loading && <Spinner />}
-          <div className="row my-5">
-            {!this.state.loading && this.state.articles.map((element) => {
-              //! every maped element should have a unique key thats why we have to asign one and jo return ho rha h usme key asign hotih
+
+        <h1 className="text-center">Inshort's - Top headlines on {this.props.category}</h1>
+        {/* {this.state.loading && <Spinner />} */}
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state
+            .totalResults}
+          loader=  { <Spinner />}
+        >
+          <div className="container">
+            <div className="row my-5">
+
+              {this.state.articles.map((element) => {
+                //! every maped element should have a unique key thats why we have to asign one and jo return ho rha h usme key asign hotih
 
 
-              return <div className="col-md-4 my-3" key={element.url}>
-                <Newsitem title={element.title} description={element.description} imgUrl={element.urlToImage} Newsurl={element.url} date ={ element.publishedAt} author = {element.author} source = {element.source.name}/>
-              </div>
+                return <div className="col-md-4 my-3" key={element.url}>
+                  <Newsitem title={element.title} description={element.description} imgUrl={element.urlToImage} Newsurl={element.url} date={element.publishedAt} author={element.author} source={element.source.name} />
+                </div>
 
-            })}
+              })}
 
+
+            </div>
           </div>
-          <div className="d-flex justify-content-between">
-            <button type="button " disabled={this.state.page <= 1} onClick={this.handlepreviousclick} className="btn btn-secondary">Previous</button>
+        </InfiniteScroll>
+{/* 
+        <div className="d-flex justify-content-between">
+          <button type="button " disabled={this.state.page <= 1} onClick={this.handlepreviousclick} className="btn btn-secondary">Previous</button>
 
-            <button type="button" id='nextpage' onClick={this.handlenextclick} className="btn btn-success" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.state.pagesize)}  >Next</button>
+          <button type="button" id='nextpage' onClick={this.handlenextclick} className="btn btn-success" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.state.pagesize)}  >Next</button>
 
-          </div>
+        </div> */}
 
-        </div>
       </div>
+
     )
   }
 }
 News.defaultProps = {
   country: 'in',
   pagesize: 9,
-  category:'health'
+  category: 'health'
 };
 News.propTypes = {
   country: PropTypes.string.isRequired,
--- React.js/insorts/src/Components/Notfound.js
+++ React.js/insorts/src/Components/Notfound.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
 
-export default class Notfound extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Not Found</h1>
-      </div>
-    )
-  }
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function Notfound() {
+  const navigate = useNavigate();
+  useEffect(()=>{
+   setTimeout(()=>{
+    navigate(-1)
+   },1000) 
+  })
+  return (
+    <h1>Not Found</h1>
+  )
 }
